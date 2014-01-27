7517bd723bf0dbc871ffa8badfd011c36d023ef1
Matthew C. Jones <mjones@isaudits.com>
2014-01-27 14:21:32 -0500

    Update file header comments; Remove copyright since in README

--- crackclient.py
+++ crackclient.py
@@ -1,25 +1,18 @@
 #! python
-# Copyright 2011 Stephen Haywood aka AverageSecurityGuy
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Crackclient makes xmlrpc calls to a crack server to automate password
-# cracking. Crackclient takes a file, a server:port combination, and a hash
-# type. The file and hash type are passed to the server and the server returns
-# an id. The id can be polled to get the results and to see if all of the
-# cracking processes are finished.
-#
+'''
+Crackclient makes xmlrpc calls to a crack server to automate password
+cracking. Crackclient takes a file, a server:port combination, and a hash
+type. The file and hash type are passed to the server and the server returns
+an id. The id can be polled to get the results and to see if all of the
+cracking processes are finished.
+
+Acceptable hash types are defined in config file associated with
+listening crackserver instance.
+
+While crackserver.py is dependent upon core modules contained in other files,
+crackclient.pl is dependent only upon standard python modules
+
+'''
 
 import xmlrpclib
 import argparse
--- crackserver.py
+++ crackserver.py
@@ -1,22 +1,7 @@
 #! /usr/local/bin/python2.7
-# Copyright 2011 Stephen Haywood aka AverageSecurityGuy
-#
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Crackserver uses the crack.py module to setup a XMLRPC server to handle
-# password cracking requests.
-#
+'''
+Launches an XMLRPC listener for password cracking requests from crackclient.py
+'''
 
 import SimpleXMLRPCServer as sxml
 import argparse
